3a0511afb6a1c1bd856ea66eff2a67d1bce7f3db
Natalie Lockyer <Natalie_lockyer@outlook.com>
2023-11-03 19:27:21 +0000

    Removed print functions

--- run.py
+++ run.py
@@ -96,9 +96,7 @@ def calculate_surplus_data(sales_row):
     print("Calculating surplus data...\n")
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(f"stock row: {stock_row}")
-    print(f"sales row: {sales_row}")
-
+    
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
